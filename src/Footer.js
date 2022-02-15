c328fd9ccdb6fa0982051a34b5bf1196d2a7dc5b
Matt Rangel <95889943+rangelMatt@users.noreply.github.com>
2022-02-14 22:31:55 -0800

    created horned beast lab01 with Features

---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render(){
+    return(
+      <footer>
+      <div id="author">Author: Matt Rangel</div>
+      &copy; CodeFellows 2022
+      </footer>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/HornedBeast.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class HornedBeast extends React.Component {
+  render(){
+    return(
+      <article>
+      <h2>{this.props.title}</h2>
+      <img src={this.props.imageUrl}
+        alt = {this.props.description}
+        title = {this.props.title}
+      />
+      <p>{this.props.description}</p>
+    </article>
+    )
+  }
+}
+
+export default HornedBeast;
